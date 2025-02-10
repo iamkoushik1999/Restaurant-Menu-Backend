b04c93b40f168a49d2efcf129c4f92869b60b14d
iamkoushik1999 <iamkoushik1999@gmail.com>
2025-02-10 22:43:12 +0530

    Fix: server test

--- server.js
+++ server.js
@@ -3,4 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 const { PORT } = process.env;
 
+app.get('/', (req, res) => res.send('Hello World!'));
+
 app.listen(PORT, () => console.log('Server is running in port:', PORT));
